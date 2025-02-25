0245e133e7f32776a810db2f8afa7ce02bd269d5
realge <q6468860681@gmail.com>
2025-02-24 20:21:51 -0600

    can render model with only one texture

---
+++ include/material/Material.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+#include <glm/glm.hpp>
+#include <memory>
+#include "../texture/Texture.h"
+
+struct Material {
+    // Base properties
+    glm::vec3 diffuseColor = glm::vec3(1.0f);
+    float alpha = 1.0f;
+    
+    // PBR properties (for future expansion)
+    float metallic = 0.0f;
+    float roughness = 0.5f;
+    glm::vec3 emissiveColor = glm::vec3(0.0f);
+    
+    // Textures
+    std::shared_ptr<Texture> diffuseTexture = nullptr;
+    std::shared_ptr<Texture> normalTexture = nullptr;
+    
+    // Settings
+    bool useTexture = false;
+    
+    // Constructor with default values
+    Material() = default;
+    
+    // Constructor with diffuse color
+    Material(const glm::vec3& color) : diffuseColor(color) {}
+    
+    // Constructor with texture
+    Material(std::shared_ptr<Texture> texture) 
+        : diffuseTexture(texture), useTexture(texture != nullptr) {}
+        
+    // Get texture binding info for updating descriptor sets
+    VkDescriptorImageInfo getTextureImageInfo() const {
+        VkDescriptorImageInfo imageInfo{};
+        if (diffuseTexture) {
+            imageInfo.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL;
+            imageInfo.imageView = diffuseTexture->getImageView();
+            imageInfo.sampler = diffuseTexture->getSampler();
+        }
+        return imageInfo;
+    }
+};
--- shaders/VertexShader.vert
+++ shaders/VertexShader.vert
@@ -1,16 +1,34 @@
 #version 450
+#extension GL_ARB_separate_shader_objects : enable
 
+// Uniform buffer with MVP matrices
 layout(binding = 0) uniform UniformBufferObject {
     mat4 model;
     mat4 view;
     mat4 proj;
 } ubo;
 
+// Vertex attributes
 layout(location = 0) in vec3 inPosition;
 layout(location = 1) in vec3 inColor;
+layout(location = 2) in vec3 inNormal;
+layout(location = 3) in vec2 inTexCoord;
+
+// Outputs to fragment shader
 layout(location = 0) out vec3 fragColor;
+layout(location = 1) out vec2 fragTexCoord;
+layout(location = 2) out vec3 fragNormal;
 
 void main() {
+    // Transform the vertex position
     gl_Position = ubo.proj * ubo.view * ubo.model * vec4(inPosition, 1.0);
+    
+    // Pass color, texture coordinates and normals to fragment shader
     fragColor = inColor;
+    fragTexCoord = inTexCoord;
+    
+    // Transform the normal using the model matrix
+    // Note: for a proper normal transformation, you should use the inverse transpose
+    // of the model matrix, but this is simplified for now
+    fragNormal = mat3(ubo.model) * inNormal;
 }
